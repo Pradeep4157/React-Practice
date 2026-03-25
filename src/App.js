def58b93dec93590c40b7a0ffa5441e2c236055f
Pradeep4157 <krp18539@gmail.com>
2026-03-25 19:38:43 +0530

    learned context api, virtual dom...

--- src/App.js
+++ src/App.js
@@ -1,54 +1,77 @@
-import "./App.css";
-import { useState } from "react";
-import { useCallback } from "react";
-import { useMemo } from "react";
-import Counter from "../src/Counter";
-function CounterButton({ event, solve, input }) {
-  console.log("BUTTON IS RENDERED");
-  return <button onClick={(event) => solve(event, input)}>Submit</button>;
-}
-function SetCounter({ solve }) {
-  const [input, handleInputChange] = useState("");
-  console.log("Set Counter is rendered");
-  return (
-    <div>
-      <form>
-        <input
-          type="number"
-          name="Enter a number : "
-          value={input}
-          onChange={(event) => handleInputChange(event.target.value)}
-        ></input>
-        <br></br>
-        <CounterButton
-          solve={solve}
-          input={input}
-          onClick={(event) => solve(event, input)}
-        ></CounterButton>
-      </form>
-    </div>
-  );
-}
+/*
+  REACT VIRTUAL DOM : 
+
+      it is a concept where react creates a virtual dom whenever we update any state and compares it with actual react dom and only 
+
+      changes the component that need re-rendering. 
+
+      also if we are creating a list dynamically using some array, we should avoid using indexes because if we insert some ele in between and update the list
+
+      it would cause errors. because of wrong indexes.
+
+      CONTEXT API : 
+
+
+
+
+*/
+import AppContext from "../src/store/app-context";
+import { useContext } from "react";
+// import "./App.css";
+// import { useState } from "react";
+// import { useCallback } from "react";
+// import { useMemo } from "react";
+// import Counter from "../src/Counter";
+// function CounterButton({ event, solve, input }) {
+//   console.log("BUTTON IS RENDERED");
+//   return <button onClick={(event) => solve(event, input)}>Submit</button>;
+// }
+// function SetCounter({ solve }) {
+//   const [input, handleInputChange] = useState("");
+//   console.log("Set Counter is rendered");
+//   return (
+
+//     <div>
+//       <form>
+//         <input
+//           type="number"
+//           name="Enter a number : "
+//           value={input}
+//           onChange={(event) => handleInputChange(event.target.value)}
+//         ></input>
+//         <br></br>
+//         <CounterButton
+//           solve={solve}
+//           input={input}
+//           onClick={(event) => solve(event, input)}
+//         ></CounterButton>
+//       </form>
+//     </div>
+//   );
+// }
 function calc(count) {
   count *= 100;
 }
 function App() {
-  console.log("APP IS RENDERED");
-  const [counter, handleCounter] = useState(0);
-  const [count, handle_random_count] = useState(null);
-  const calculate_random = useMemo(() => {
-    calc(count);
-  }, [count]);
+  const message = useContext(AppContext);
+  // console.log("APP IS RENDERED");
+  // const [counter, handleCounter] = useState(0);
+  // const [count, handle_random_count] = useState(null);
+  // const calculate_random = useMemo(() => {
+  //   calc(count);
+  // }, [count]);
 
-  const solve = useCallback((event, input) => {
-    event.preventDefault();
-    handleCounter(input);
-  }, []);
+  // const solve = useCallback((event, input) => {
+  //   event.preventDefault();
+  //   handleCounter(input);
+  // }, []);
+
   return (
-    <div>
-      <Counter counter={counter}> </Counter>
-      <SetCounter solve={solve}></SetCounter>
-    </div>
+    <div>{message.message}</div>
+    // <div>
+    //   <Counter counter={counter}> </Counter>
+    //   <SetCounter solve={solve}></SetCounter>
+    // </div>
   );
 }
 
